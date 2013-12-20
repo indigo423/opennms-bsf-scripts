e4308842d52734c727b7fa6d9c9d8daf5408cdea
Ronny Trommer <ronny@opennms.org>
2013-12-20 14:16:02 +0100

    Initial commit

---
+++ DNS-Blacklist-Monitor/src/SpamBlackListMonitor.groovy
@@ -0,0 +1,10 @@
+/**
+ * <p>SpamBlackListMonitor class.</p>
+ *
+ * @author <a href="mailto:ronny@opennms.org">Ronny Trommer</a>
+ * @version $Id: $
+ * @since 1.0-SNAPSHOT
+ */
+class SpamBlackListMonitor {
+
+}
